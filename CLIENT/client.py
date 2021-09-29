3bfbce9d93a214c79091afc59ece3e969fd23371
hrichharms <freelanceharms@gmail.com>
2021-09-29 02:10:46 -0500

    updated client code

--- CLIENT/client.py
+++ CLIENT/client.py
@@ -2,30 +2,50 @@ from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
 import cv2
+import socket
+import datetime
 
-# get camera object
-camera = PiCamera()
 
-# set camera resolution
-camera.resolution = (640, 480)
+PORT = 8000
+RESOLUTION = (640, 480)
 
-# set fps
-camera.framerate = 32
 
-# Generates a 3D RGB array and stores it in rawCapture
-raw_capture = PiRGBArray(camera, size=(640, 480))
+def debug(x: str):
+    print(str(datetime.datetime.now()).split()[1].split(".")[0], x)
 
-# Wait a certain number of seconds to allow the camera time to warmup
-time.sleep(0.1)
 
-# Capture frames continuously from the camera
-for frame in camera.capture_continuous(raw_capture, format="bgr", use_video_port=True):
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    return s.getsockname()[0]
 
-    # Grab the raw NumPy array representing the image
-    image = frame.array
 
-    # Display the frame using OpenCV
-    cv2.imshow("Frame", image)
+def serialize():
+    pass
 
-    # Clear the stream in preparation for the next frame
-    raw_capture.truncate(0)
+
+if __name__ == "__main__":
+
+    # get camera object
+    camera = PiCamera()
+
+    # set camera resolution
+    camera.resolution = RESOLUTION
+
+    # set fps
+    camera.framerate = 32
+
+    # array to store camera frame data
+    raw_capture = PiRGBArray(camera, size=RESOLUTION)
+
+    # continous capture loop
+    for frame in camera.capture_continuous(raw_capture, format="bgr", use_video_port=True):
+
+        print(type(frame.array))
+        exit()
+
+        # serialize and send the current frame to the viewing server
+        image = frame.array
+
+        # clear raw capture array for next frame
+        raw_capture.truncate(0)
